ac39378377aaa5251dfa9014a6837238257a7de6
TakumaInoue <inouetakuma19921228@gmail.com>
2023-06-05 01:09:02 +0900

    ボタンを追加・編集

--- App.tsx
+++ App.tsx
@@ -6,24 +6,46 @@ import {
   TextInput,
   Button,
   TouchableHighlight,
+  Alert,
 } from 'react-native';
 
 const Cat = () => {
   const name = 'taro';
+
+  const createTwoButtonAlert = () =>
+    Alert.alert('Alert Title', 'My Alert Msg', [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {text: 'OK', onPress: () => console.log('OK Pressed')},
+    ]);
+
+  const createThreeButtonAlert = () =>
+    Alert.alert('Alert Title', 'My Alert Msg', [
+      {
+        text: 'Ask me later',
+        onPress: () => console.log('Ask me later pressed'),
+      },
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {text: 'OK', onPress: () => console.log('OK Pressed')},
+    ]);
+
   return (
     <View style={styles.container}>
       <Text style={styles.label}>Hello, I am...</Text>
-      <TextInput style={styles.input} defaultValue="Name me!" />
-      <Button
-        title="Press me"
-        onPress={() =>
-          alert('デフォルトコンポーネントのボタンが押されました！')
-        }
-      />
+      <TextInput style={styles.input} defaultValue="Enter your name!" />
+      <Button title={'TwoButtonAlert'} onPress={createTwoButtonAlert} />
+      <Button title={'ThreeButtonAlert'} onPress={createThreeButtonAlert} />
+
       <TouchableHighlight
         style={styles.button4}
-        onPress={() => console.log('press4')}
-        onPress={() => alert('ボタン4が押されました！')}
+        onPress={() => Alert.alert('ボタン4が押されました！')}
         activeOpacity={0.6}
         underlayColor="pink">
         <Text style={styles.fontStyle}>button4</Text>
@@ -46,6 +68,16 @@ const styles = StyleSheet.create({
     marginTop: 100,
     textAlign: 'center',
     backgroundColor: 'lightgray',
+  },
+  button1: {
+    backgroundColor: 'blue',
+    padding: 20,
+    width: '40%',
+    alignSelf: 'center',
+    marginTop: 50,
+    borderWidth: 1,
+    borderColor: 'white',
+    borderRadius: 15,
   },
   button4: {
     backgroundColor: 'tomato',
