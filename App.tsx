88fc4c4da1f9c07c47442c31b186be3baeb38c4b
TakumaInoue <inouetakuma19921228@gmail.com>
2023-06-11 17:16:51 +0900

    SectionListを追加

--- App.tsx
+++ App.tsx
@@ -1,99 +1,54 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Button,
-  TouchableHighlight,
-  Alert,
-} from 'react-native';
-
-const Cat = () => {
-  const name = 'taro';
-
-  const createTwoButtonAlert = () =>
-    Alert.alert('Alert Title', 'My Alert Msg', [
-      {
-        text: 'Cancel',
-        onPress: () => console.log('Cancel Pressed'),
-        style: 'cancel',
-      },
-      {text: 'OK', onPress: () => console.log('OK Pressed')},
-    ]);
-
-  const createThreeButtonAlert = () =>
-    Alert.alert('Alert Title', 'My Alert Msg', [
-      {
-        text: 'Ask me later',
-        onPress: () => console.log('Ask me later pressed'),
-      },
-      {
-        text: 'Cancel',
-        onPress: () => console.log('Cancel Pressed'),
-        style: 'cancel',
-      },
-      {text: 'OK', onPress: () => console.log('OK Pressed')},
-    ]);
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.label}>Hello, I am...</Text>
-      <TextInput style={styles.input} defaultValue="Enter your name!" />
-      <Button title={'TwoButtonAlert'} onPress={createTwoButtonAlert} />
-      <Button title={'ThreeButtonAlert'} onPress={createThreeButtonAlert} />
-
-      <TouchableHighlight
-        style={styles.button4}
-        onPress={() => Alert.alert('ボタン4が押されました！')}
-        activeOpacity={0.6}
-        underlayColor="pink">
-        <Text style={styles.fontStyle}>button4</Text>
-      </TouchableHighlight>
-    </View>
-  );
-};
+import {SectionList, StyleSheet, Text, View} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 22,
   },
-  label: {
-    fontSize: 40,
-    marginTop: 100,
-    backgroundColor: 'lightblue',
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
   },
-  input: {
-    fontSize: 40,
-    marginTop: 100,
-    textAlign: 'center',
-    backgroundColor: 'lightgray',
-  },
-  button1: {
-    backgroundColor: 'blue',
-    padding: 20,
-    width: '40%',
-    alignSelf: 'center',
-    marginTop: 50,
-    borderWidth: 1,
-    borderColor: 'white',
-    borderRadius: 15,
-  },
-  button4: {
-    backgroundColor: 'tomato',
-    padding: 20,
-    width: '40%',
-    alignSelf: 'center',
-    marginTop: 50,
-    borderWidth: 1,
-    borderColor: 'white',
-    borderRadius: 15,
-  },
-  fontStyle: {
-    color: 'white',
-    alignSelf: 'center',
-    fontSize: 24,
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
 
-export default Cat;
+const SectionListBasics = () => {
+  return (
+    <View style={styles.container}>
+      <SectionList
+        sections={[
+          {title: 'D', data: ['Devin', 'Dan', 'Dominic']},
+          {
+            title: 'J',
+            data: [
+              'Jackson',
+              'James',
+              'Jillian',
+              'Jimmy',
+              'Joel',
+              'John',
+              'Julie',
+            ],
+          },
+        ]}
+        renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+        renderSectionHeader={({section}) => (
+          <Text style={styles.sectionHeader}>{section.title}</Text>
+        )}
+        keyExtractor={item => `basicListEntry-${item}`}
+      />
+    </View>
+  );
+};
+
+export default SectionListBasics;
